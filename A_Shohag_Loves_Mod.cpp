8b7d8ae2a16a65b0eced98b2c946fb04d85079cd
kunal7236 <kunakashyap277@gmail.com>
2025-08-05 13:37:56 +0530

    2039A

---
+++ A_Shohag_Loves_Mod.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+            cout << (i << 1) - 1 << " ";
+        cout << endl;
+    }
+}
